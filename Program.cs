be5cd09a8c17ec73c50b8d93a1fb3bb4dad5dc21
NicolasOsmundo <nicolasosmundo@hotmail.com>
2021-03-26 18:32:22 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class ClasseBase
+{
+    public void Metodo()
+    {
+        Console.WriteLine("Método ClasseBase");
+    }
+}
+    public class Derivada : ClasseBase
+    {
+        public void Metodo()
+        {
+            Console.WriteLine("Método Derivado");
+
+        }
+    }
+public class Program
+{
+    static void Main(string[] args)
+    {
+        ClasseBase a = new ClasseBase();
+        a.Metodo();
+        Derivada b = new Derivada();
+        b.Metodo();
+        ClasseBase c = new Derivada();
+        c.Metodo();
+        Console.Read();
+    }
+}
+    
